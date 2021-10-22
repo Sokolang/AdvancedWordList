71a35ff8e51ba1c8dca281e00ce9a7cd2fdc37b6
Anzhelika Sokolova <skelvin.pit@icloud.com>
2021-10-22 17:21:17 +0200

    Editing items

---
+++ AdvancedWordList/ItemRowView.swift
@@ -0,0 +1,24 @@
+//
+//  ItemRowView.swift
+//  AdvancedWordList
+//
+//  Created by Anzhellika Sokolova on 22.10.2021.
+//
+
+import SwiftUI
+
+struct ItemRowView: View {
+    @ObservedObject var item: Item
+    
+    var body: some View {
+        NavigationLink(destination: EditItemView(item: item)) {
+            Text(item.itemTitle)
+        }
+    }
+}
+
+struct ItemRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRowView(item: Item.example)
+    }
+}
