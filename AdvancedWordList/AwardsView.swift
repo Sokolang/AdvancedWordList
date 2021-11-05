bce222ac07881a20569189b3d6aa40d63bf408be
Anzhelika Sokolova <skelvin.pit@icloud.com>
2021-11-05 15:18:22 +0200

    Reading awards JSON

---
+++ AdvancedWordList/AwardsView.swift
@@ -0,0 +1,41 @@
+//
+//  AwardsView.swift
+//  AdvancedWordList
+//
+//  Created by Anzhellika Sokolova on 05.11.2021.
+//
+
+import SwiftUI
+
+struct AwardsView: View {
+    static let tag: String? = "Awards"
+    var columns: [GridItem] {
+        [GridItem(.adaptive(minimum: 100, maximum: 100))]
+    }
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) { ForEach(Award.allAwards) { award in
+                    Button {
+                        // no action yet
+                    } label: {
+                        Image(systemName: award.image)
+                            .resizable()
+                            .scaledToFit()
+                            .padding()
+                            .frame(width: 100, height: 100)
+                            .foregroundColor(Color.secondary.opacity(0.5))
+                    }
+                }
+                }
+            }
+            .navigationTitle("Awards")
+        }
+    }
+}
+
+struct AwardsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AwardsView()
+    }
+}
