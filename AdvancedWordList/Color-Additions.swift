aa9ad47f744aea96b5e197e6d0dea7bb60bbfa8a
Anzhelika Sokolova <skelvin.pit@icloud.com>
2021-11-06 12:52:06 +0200

    Creating the Home view

---
+++ AdvancedWordList/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  AdvancedWordList
+//
+//  Created by Anzhellika Sokolova on 06.11.2021.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
